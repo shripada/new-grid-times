5c04c2dd7df79087d6fcabf3bb75f48ba39de015
Shripada Hebbar <shripada.hebbar@codecraft.co.in>
2023-02-15 17:32:59 +0530

    solved problem 1 - finetuning header to be responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,11 +21,16 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
-          <ActionGroup>
+          <DesktopHeader> <Logo /></DesktopHeader>
+          <RightActionGroupMobileTablet>
             <button>
               <User size={24} />
             </button>
-          </ActionGroup>
+          </RightActionGroupMobileTablet>
+
+          <RightActionGroup><Button>Subscribe</Button>
+          <Link>Already a subscriber?</Link>
+          </RightActionGroup>
         </Row>
       </SuperHeader>
       <MainHeader>
@@ -39,6 +44,17 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media ${QUERIES.laptopAndUp}{
+    background: white;
+  }
+
+  &  svg {
+    stroke: white;
+    @media ${QUERIES.laptopAndUp}{
+    stroke: black;
+  }
+   
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,53 @@ const ActionGroup = styled.div`
   }
 `;
 
+const RightActionGroupMobileTablet = styled(ActionGroup)`
+  @media ${QUERIES.laptopAndUp}{
+    display:none;
+  }
+`
+
+const RightActionGroup = styled(ActionGroup)`
+  display:none;
+  color:black;
+  @media ${QUERIES.laptopAndUp}{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap:0;
+  }
+`
+
+const Link = styled.a`
+  margin-top:8px;
+  font-family: var(--font-family-serif);
+  font-size: calc(14rem/16);
+  font-style: italic;
+  text-decoration: underline;
+  cursor: pointer;
+`;
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+   @media ${QUERIES.laptopAndUp} {
+     display:none;
+  } 
 `;
 
+const DesktopHeader = styled(MaxWidthWrapper)`
+margin-top: 32px;
+margin-bottom: 48px;
+display:none;
+color:black;
+@media ${QUERIES.laptopAndUp} {
+   display:flex;
+   align-items: center;
+   justify-content: center;
+ 
+} `
 export default Header;
